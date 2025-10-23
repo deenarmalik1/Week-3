f809b0a29057518c60403c67e6ccd5d64ac3d81d
Malik deenar <deenarmalik49@gmail.com>
2025-10-23 23:04:32 +0530

    First Commit

---
+++ Day1/Javascript.js
@@ -0,0 +1,13 @@
+//Javascript is a high level programming language that is primarily used for client-side scripting on the web. its often used to create interactive web pages, web applications and mobile applications
+
+//Javascript is called a scripting language because of the following:
+
+// : javascript code is typically executed line by line by an interpretor without the need for compilation
+// : Javascript's dynamic typing allows for flexibility in coding,Which is characteristic of scripting languages
+// : Javascript is often used to automate tasks,such as manipulating web pages,handling user interactions and creating dynamic effects
+
+// Javascript is Called programming language because of the following:
+
+// : Javascript supports structured programming concepts like variables,control structures,functions, and object-oriented programming
+// : Javascript is used to build complex web applications,mobile apps etc which require programming logic
+// : Javascript has vast eco-system of libraries and frameworks(eg, React,Angular,Vue.js) that enable developers to build applications
